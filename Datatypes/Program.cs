f6eba3aa5d5f1604833a9939dbca8adc85e2d7d1
Opilane TTHK <opilane@tthk.ee>
2025-02-26 11:28:39 +0200

    Update Program.cs

--- Datatypes/Program.cs
+++ Datatypes/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Data Types");
 
+            //täisnumber
+            Console.WriteLine("Sisestage täisnumber.");
+            string bigNumber = Console.ReadLine();
+            int.Parse(bigNumber);
+            Console.WriteLine("Teie täisnumber on: "+bigNumber);
 
-
-
+            //komaga number
+            Console.WriteLine("Sisestage komaga number.");
+            string komaNumber = Console.ReadLine();
+            double.Parse(komaNumber);
+            Console.WriteLine("Teie komaga number on: "+komaNumber);
 
         }
     }
