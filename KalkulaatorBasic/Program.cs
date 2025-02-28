815d349be78493af5cad4ba9655907ced9cf58c9
Henri-K20 <henrikimmo@gmail.com>
2025-02-28 19:16:12 +0200

    KalkulaatorBasic
    Made KalkulaatorBasic.

---
+++ KalkulaatorBasic/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulaatorBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = 0;
+            double second = 0;
+            
+            //tervitame
+            Console.WriteLine("Tere! Mina olen kalkulaator!");
+
+            //küsime esimest numbrit
+            Console.Write("Sisestage esimene number: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            //küsime teist numbrit
+            Console.Write("Sisestage teine number: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            //küsime kas soovib liita/lahutada vms
+            Console.WriteLine("Valige üks:");
+            Console.WriteLine("+ : liida");
+            Console.WriteLine("- : lahuta");
+            Console.WriteLine("* : korruta");
+            Console.WriteLine("/ : jaga");
+            string valik = Console.ReadLine();
+            if (valik == "+")
+            {
+                double vaste = first + second;
+                Console.WriteLine("Vastus on: " + vaste + " !");
+            }
+            else if (valik == "-")
+            {
+                double vaste = first - second;
+                Console.WriteLine("Vastus on: " + vaste + " !");
+            }
+            else if (valik == "*")
+            {
+                double vaste = first * second;
+                Console.WriteLine("Vastus on: " + vaste + " !");
+            }
+            else if (valik == "/")
+            {
+                double vaste = first / second;
+                Console.WriteLine("Vastus on: " + vaste + " !");
+            }
+
+        }
+    }
+}
