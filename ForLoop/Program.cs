26f6da1980ef1fd52dc236371976d947f6435490
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:17:50 +0200

    ForLoop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nom = 3;
+
+            for (int i = 1; i < nom + 1;i ++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
