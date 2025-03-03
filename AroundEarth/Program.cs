c7eb4a82183b3554774882aaecc23b5b58ca963e
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:04:57 +0200

    AroundEarth stuff

---
+++ AroundEarth/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AroundEarth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double coindiameeter = 0.02575; //m      
+            double maaraadius = 6378000; //m           
+            double ümbermõõt = (2 * Math.PI * maaraadius);
+            double coinamount = ümbermõõt / coindiameeter;
+
+            Console.WriteLine("Maa ümbermõõt on " + ümbermõõt/1000 + " kilomeetrit."); // converted to km
+            Console.WriteLine("Ümber maa läheb " + coinamount + " 2 eurost münti.");
+        }
+    }
+}
