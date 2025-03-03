12d68369bc43c0c801e4b16dcb6e36cf2b72a7c6
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:30:52 +0200

    ForLoop-2-KillKoll

---
+++ ForLoop-2-KillKoll/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForLoop_2_KillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            for (int y = 0; y < 3; y++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("kill-koll");
+                    if (i == 1)
+                    {
+                        for (int j = 0; j < 1; j++)
+                        {
+                            Console.WriteLine("killadi-koll");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
