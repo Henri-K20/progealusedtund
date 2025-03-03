5d717b0f16d7c96cabd6e150085f884ac02d080a
Opilane TTHK <opilane@tthk.ee>
2025-03-03 14:16:57 +0200

    learning brackets ig...
    hang me

---
+++ Brackets/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kui palju on 3 + 8 / (4 - 2) * 4?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            double calculation2 = (3 + 8) / (4 - 2) * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = ((3.0 + 8.0) / (4.0 - 2.0) * 4.0);
+            //arvuti arvutab, järgides sulgi ja arvutusmärke
+
+            Console.WriteLine("Vastus: " + calculation1);
+            Console.WriteLine("Vastus: " + calculation2);
+            Console.WriteLine("Vastus: " + calculation3);
+            Console.WriteLine("Vastus: " + calculation4);
+        }
+    }
+}
