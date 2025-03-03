9d7a0e0c870660f0ae6af4e2a27e3d9c4324a408
Opilane TTHK <opilane@tthk.ee>
2025-03-03 13:45:55 +0200

    IfandElse oppimine

---
+++ StringIfAndElse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StringIfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Mis on teie nimi?");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string nimi = Console.ReadLine();
+            if (string.IsNullOrEmpty(nimi) == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Err. See ei ole nimi.");
+                Console.Beep();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Sinu nimi on " + nimi);
+            }
+        }
+    }
+}
