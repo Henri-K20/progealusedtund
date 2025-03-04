2006348fcfffe7bed693e0ee9478c9690b12c642
Opilane TTHK <opilane@tthk.ee>
2025-03-04 10:48:30 +0200

    paaris-paaritu töö

---
+++ IfElseOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IfElseOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0 )
+            {
+                Console.WriteLine("Paaris");
+            }
+            else if (number % 2 != 0 )
+            {
+                Console.WriteLine("Paaritu");
+            }
+        }
+    }
+}
