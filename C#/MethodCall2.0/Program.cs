35cef61d4e08e3a796891d1540c7bbb78a3bf624
Opilane TTHK <opilane@tthk.ee>
2025-03-04 11:36:52 +0200

    Method call with OddOrEven

---
+++ C#/MethodCall2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodCall2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage arv!");
+            int arv = int.Parse(Console.ReadLine());
+
+            if (arv % 2 == 0)
+            {
+                paaris();
+            }
+            else if (arv % 2 != 0)
+            {
+                paaritu();
+            }
+        }
+        static void paaris()
+        {           
+            Console.WriteLine("See on paaris arv!");
+        }
+        static void paaritu()
+        {
+            Console.WriteLine("See on paaritu arv!");
+        }
+    }
+}
