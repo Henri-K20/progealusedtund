f39753c979159ccfe6646edcfd3b17c56f717df5
Opilane TTHK <opilane@tthk.ee>
2025-03-04 11:24:34 +0200

    making methods

---
+++ C#/MethodCall/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //meetodite tegemine sama, kui pythonis funktsiooni definimine
+            //C# kasutab static void
+            //python kasutab lihtsalt def
+
+
+            Console.WriteLine("Hello, World!");
+            //calling jalgratas
+            Jalgratas();
+        }
+        static void Jalgratas()
+        {
+            Console.WriteLine("Nüüd oled jalgratta meetodis");
+        }
+    }
+}
