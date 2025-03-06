7052f365c7e7438c649c61690d63880e1d7df153
Opilane TTHK <opilane@tthk.ee>
2025-03-06 09:05:26 +0200

    RandomNumbersSwitch

---
+++ C#/RandomNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+
+namespace RandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(1,7);
+            switch (number)
+            {
+                case int i when (i >= 1 && i <= 6):                 
+                    Console.WriteLine(number);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Err");
+                    break;
+            }       
+        }
+    }
+}
