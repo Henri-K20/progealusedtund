98512f9f593cdb361d0c1abe2167d57b62c57eb1
Henri-K20 <henrikimmo@gmail.com>
2025-03-24 11:32:38 +0200

    w match case

---
+++ Python/LemmikToitMatchCase.py
@@ -0,0 +1,29 @@
+valikud = ["makaron", "kiluvõileib", "hakklihakaste"]
+valik = ""
+i = 5
+
+while True:
+    valik = input("""
+########################################
+Sisestage oma lemmik toit siit valikust:
+Makaron
+Kiluvõileib
+Hakklihakaste
+########################################
+:""").lower()
+    if i == 0:
+        print("ok, kui sa midagi ei taha siis aidaa!")
+        break
+    match valik:
+        case "makaron":
+            print("Sinu lemmiksöök on paljas makaron, nõme")
+            break
+        case "kiluvõileib":
+            print("Ohh, eesti värk, naiss")
+            break
+        case "hakklihakaste":
+            print("Eh, kinda mid")
+            break
+        case _:
+            input("Seda ei ole valikus! Vajuta enter, et uuesti valida...\n:")
+            i -= 1
