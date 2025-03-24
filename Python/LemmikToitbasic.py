457ff7fc584c13a6536143405d8d506aea39efa7
Henri-K20 <henrikimmo@gmail.com>
2025-03-24 10:52:49 +0200

    meh

---
+++ Python/LemmikToitbasic.py
@@ -0,0 +1,22 @@
+valikud = ["makaron", "kiluvõileib", "hakklihakaste"]
+valik = ""
+
+while True:
+    valik = input("""
+########################################
+Sisestage oma lemmik toit siit valikust:
+Makaron
+Kiluvõileib
+Hakklihakaste
+########################################
+:""").lower()
+    if valik in valikud:
+        if valik == "makaron":
+            print("Sinu lemmiksöök on paljas makaron, nõme")
+        elif valik == "kiluvõileib":
+            print("Ohh, eesti värk, naiss")
+        elif valik == "hakklihakaste":
+            print("Eh, kinda mid")
+        break
+    else:
+        input("Seda ei ole valikus! Vajuta enter, et uuesti proovida...\n:")
