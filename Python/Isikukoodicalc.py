e4061ae0057dc28d4e22104b672c88ede313dcee
Opilane TTHK <opilane@tthk.ee>
2025-04-07 14:19:23 +0300

    anxiety

--- Python/Isikukoodicalc.py
+++ Python/Isikukoodicalc.py
@@ -2,7 +2,7 @@ from random import *
 vanus = 0
 bday = 0
 bmonth = 0
-byear = 0
+byear = ""
 sugu = ""
 i = True
 idkood = ""
@@ -11,7 +11,7 @@ while i:
         vanus = int(input("Sisestage oma vanus.\n:"))
         bday = int(input("Sisestage oma sünnipäev. \n:"))
         bmonth = int(input("Sisestage oma sünnikuu. (Kuu number) \n:"))
-        byear = int(input("Sisestage oma sünniaasta. (Viimased kaks numbrit)\n:"))
+        byear = input("Sisestage oma sünniaasta. (Viimased kaks numbrit)\n:")
         sugu = input("Sisestage oma sugu. m/n \n:").lower()
     except ValueError:
         input("Väär andmetüüp! Sisestage ainult arve. Vajutage enter, et uuesti proovida.")
@@ -24,10 +24,7 @@ if i == False:
     else:
         sugu = choice([4,6])
     if len(str(bmonth)) == 1:
-        bmonth = str(bmonth)
-        bmonth = bmonth.zfill(len(2))
+        bmonth = "0" + str(bmonth)
     idkood = str(sugu)+str(byear)+str(bmonth)+str(bday)+str(randint(1000,9999))
-    
-    print(f"Palun, siin on teie isikukood.\n{idkood}")
-    
-    
+    nimi = input("Sisestage oma ees- ja perekonnanimi.\n:").title()
+    print(f"Palun, {nimi} siin on teie isikukood.\n{idkood}")
