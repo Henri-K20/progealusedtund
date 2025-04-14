2e97ed3c3de2f5d871b1dc12dc4f99390b015f09
Opilane TTHK <opilane@tthk.ee>
2025-04-14 15:38:09 +0300

    asdasd

--- Python/liikuvpall.py
+++ Python/liikuvpall.py
@@ -7,34 +7,57 @@ raam.title("WOW!")
 tahvel = Canvas(raam,width=600,height=600)
 px = 0
 py = 0
+
+
 pall = tahvel.create_oval(275,275,325,325,fill="black")
+#sein = tahvel.create_rectangle(0,0,100,600,fill="black")
 
-while not is_pressed("q"):
+
+def update():
+    raam.update()
+    tahvel.pack()
+
+def up():
+    tahvel.move(pall,0,-5)
+    raam.after(5)
+
+def down():
+    tahvel.move(pall,0,5)
+    raam.after(5)
+    
+def left():
+    tahvel.move(pall,-5,0)
+    raam.after(5)
+
+def right():
+    tahvel.move(pall,5,0)
+    raam.after(5)
+
+while True:
+    if is_pressed("q"):
+        quit()
     if is_pressed("w"):
-        tahvel.move(pall,px,py-5)
-        tahvel.pack()
-        raam.update()
-        raam.after(5)
-        
+        up()
     if is_pressed("s"):
-        tahvel.move(pall,px,py+5)
-        tahvel.pack()
-        raam.update()
-        raam.after(5)
-
+        down()
     if is_pressed("a"):
-        tahvel.move(pall,px-5,py)
-        tahvel.pack()
-        raam.update()
-        raam.after(5)
-        
-    if is_pressed("a"):
+       left()
+    if is_pressed("d"):
+        right()
+    if px >= 100:
         tahvel.move(pall,px+5,py)
-        tahvel.pack()
-        raam.update()
-        raam.after(5)
 
-tahvel.pack()
+
+
+
+
+
+
+
+
+
+    print(f"x: {px} , y: {py}")
+    update()
 raam.mainloop()
 
 
