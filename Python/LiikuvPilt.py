07c8713508901b8c86bb0939fb3c41feb09d9fc9
Opilane TTHK <opilane@tthk.ee>
2025-04-15 09:56:05 +0300

    hmm

---
+++ Python/LiikuvPilt.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+raam = Tk()
+raam.title("WOW!")
+tahvel = Canvas(raam,width=1000,height=1000)
+px = 0
+py = 0
+
+
+
+objekt = input("""
+#######################
+ Valige lemmik objekt:
+
+1. Pall
+2. Ruubiku-Kuubik
+3. Vihmavari
+4. Vape
+
+Sisestage valiku arv.
+:""")
+
+img = 
+
+
+
+update()
+raam.mainloop()
--- Python/tkinterGUIwithgrid.py
+++ Python/tkinterGUIwithgrid.py
@@ -19,8 +19,7 @@ nimi = ttk.Entry(raam)
 nimi.grid(column=1,row=0,padx=5,pady=5,sticky=(N,W,E))
 
 #greeting nupp
-nupp = ttk.Button(raam,text="Customer greeting", command=tervita)
-nupp.grid(column=1,row=1,padx=5,pady=5,sticky=(N,W,E,S))
+nupp = ttk.Button(raam,text="Customer greeting", command=tervita).grid(column=1,row=1,padx=5,pady=5,sticky=(N,W,E,S))
 
 raam.columnconfigure(1,weight=1)
 raam.rowconfigure(1,weight=1)
