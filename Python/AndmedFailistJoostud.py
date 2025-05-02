b7b5ad2b0a17c2e6968cc1383a35c82e7e2d6961
Opilane TTHK <opilane@tthk.ee>
2025-05-02 09:49:57 +0300

    Update AndmedFailistJoostud.py

--- Python/AndmedFailistJoostud.py
+++ Python/AndmedFailistJoostud.py
@@ -1,4 +1,17 @@
 fail = open("Andmed\joostud.txt", encoding="UTF-8")
+# for rida in fail:
+#     if float(rida) > 10:
+#         print(float(rida))
+# fail.close()
+kokku = []
+keskmine = 0
 for rida in fail:
-    if float(rida) > 10:
-        print(float(rida))
+    kokku.append(rida)
+fail.close()
+print("Siin on kõik tulemused:\n", *kokku)
+print("Siin on tulemused, mis on 10km või alla:")
+for arv in kokku:
+    keskmine+=float(arv)
+    if float(arv) <= 10:
+        print(arv.strip())
+print(f"\nJooksude keskmine on: {round(float(arv)/len(kokku),2)} km")
