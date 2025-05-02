27b345761d835d76200cb54da36e70db34c8a28d
Opilane TTHK <opilane@tthk.ee>
2025-05-02 11:09:30 +0300

    Update lauluProgramm.py

--- Python/lauluProgramm.py
+++ Python/lauluProgramm.py
@@ -1,7 +1,29 @@
-fail = open("Andmed/laulusõnad.txt")
+fail = open("Andmed\laulusõnad.txt", encoding="UTF-8")
 sõnad = []
+kokku = 0
+vaste = ""
 
-for sõna in fail:
-    sõnad.append(sõna)
-print(sõnad)
+for rida in fail:
+    sõnad.append(rida.strip())
+    kokku += rida.count("a") + rida.count("e") + rida.count("i") + rida.count("o") + rida.count("u") + rida.count("õ") + rida.count("ä") + rida.count("ö") + rida.count("ü") + rida.count("y")
 fail.close()
+
+print("Laulusõnad on:\n", *sõnad, sep="\n")
+print(f"\nTäishäälikuid on: {kokku}")
+print(f"\nSõnad, mis on pikemad kui 7 tähte on:")
+
+for rida in sõnad:
+    for sõna in rida.split():
+        if len(sõna) > 7:
+            print(sõna)
+for i in range(4):
+    sõnad.append(input(f"\nSisesta uus rida nr {i+1}.\n:"))
+
+while vaste != "ei" and vaste != "jah":
+    vaste = input("\nKas soovite näha sõnu uuesti? jah/ei\n:").lower()
+    if vaste == "ei":
+        print("Arusaadav.")
+    elif vaste == "jah":
+        print(*sõnad,sep="\n")
+    else:
+        input("Väär sisestus! Vajutage enter, et uuesti proovida...")
